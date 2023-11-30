3eb9c53808edbd0aea2e015c8f35a29244a4195c
romainChuat <romain.chuat@outlook.com>
2023-11-30 13:16:55 +0100

    like_comment after request/payment

--- cypress/tests/test_tp/like_comment.spec.ts
+++ cypress/tests/test_tp/like_comment.spec.ts
@@ -5,7 +5,7 @@ describe('Like, comment', () => {
     var name = "Edgar";
     var lastName = "Johns";
 
-    before(function () {
+    beforeEach(function () {
         cy.visit('http://localhost:3000/signin');
         cy.get('input[name="username"]').type('Katharina_Bernier');
         cy.get('input[name="password"]').type("s3cret");
@@ -39,33 +39,107 @@ describe('Like, comment', () => {
         cy.get('[data-test^="transaction-like-count"]').should("contain", '0');
         cy.get('[data-test^="transaction-like-button-"]').click();
         cy.get('[data-test^="transaction-like-count"]').should("contain", '1');
-    });
+        cy.get('[data-test^="transaction-like-button-"]').should("have.attr", "disabled");
 
+    });
 
     it('Like after request', () => {
-  
+        cy.get('[data-test="nav-top-new-transaction"]').click();
+        cy.get('[data-test^="user-list-search-input"]').focus().type("Tavares_Barrows");
+        var receiverName = "Arely"; 
+        var recieverLastName =  "Kertzmann";
+        cy.get('[data-test^="user-list-item-"]:first').should("contain",receiverName+" "+recieverLastName).click(); 
+        var transactionName = "Une requete de test";
+        var amount = "185";
+        cy.get('input[name="amount"]').type(amount);
+        cy.get('input[name="description"]').type(transactionName);
+        cy.get('[data-test="transaction-create-submit-request"]').click();
+        cy.get('[data-test="new-transaction-return-to-transactions"]').click();
+        // recherche le payement
+        cy.get('[data-test="nav-personal-tab"]').click();
+        cy.get('li :first').click();
+
+        cy.get('[data-test^="transaction-sender-"]').should("contain", name+' '+lastName);
+        cy.get('[data-test^="transaction-action-"]').should("contain", 'requested');
+        cy.get('[data-test^="transaction-receiver-"]').should("contain", receiverName+' '+recieverLastName);
+
+        cy.get('[data-test^="transaction-description"]').should("contain", transactionName);
+        cy.get('[data-test^="transaction-amount-"]').should("contain", "+$"+amount);
+        
+        cy.get('[data-test^="transaction-like-count"]').should("contain", '0');
+        cy.get('[data-test^="transaction-like-button-"]').click();
+        cy.get('[data-test^="transaction-like-count"]').should("contain", '1');
+        cy.get('[data-test^="transaction-like-button-"]').should("have.attr", "disabled");
     });
 
-    //like in list
 
-    // comment after payement
+    // comment after request
+    it('Comment after request', () => {
+        cy.get('[data-test="nav-top-new-transaction"]').click();
+        cy.get('[data-test^="user-list-search-input"]').focus().type("Tavares_Barrows");
+        var receiverName = "Arely"; 
+        var recieverLastName =  "Kertzmann";
+        cy.get('[data-test^="user-list-item-"]:first').should("contain",receiverName+" "+recieverLastName).click(); 
+        var transactionName = "Une requete de test";
+        var amount = "185";
+        cy.get('input[name="amount"]').type(amount);
+        cy.get('input[name="description"]').type(transactionName);
+        cy.get('[data-test="transaction-create-submit-request"]').click();
+        cy.get('[data-test="new-transaction-return-to-transactions"]').click();
+        // recherche le payement
+        cy.get('[data-test="nav-personal-tab"]').click();
+        cy.get('li :first').click();
 
-    //comment after resquest
+        cy.get('[data-test^="transaction-sender-"]').should("contain", name+' '+lastName);
+        cy.get('[data-test^="transaction-action-"]').should("contain", 'requested');
+        cy.get('[data-test^="transaction-receiver-"]').should("contain", receiverName+' '+recieverLastName);
 
-    //comment in list
+        cy.get('[data-test^="transaction-description"]').should("contain", transactionName);
+        cy.get('[data-test^="transaction-amount-"]').should("contain", "+$"+amount);
+        var commentaire = "Mon commentaire de test"
+        cy.get('[data-test^="transaction-comment-input"]').type(commentaire+'{enter}');
+        cy.get('[data-test^="comment-list-item"]:last').should("contain", commentaire);
+
+    });
 
 
+    it('Comment after payement', () => {
+        cy.get('[data-test="nav-top-new-transaction"]').click();
+        cy.get('[data-test^="user-list-search-input"]').focus().type("Tavares_Barrows");
+        var receiverName = "Arely"; 
+        var recieverLastName =  "Kertzmann";
+        cy.get('[data-test^="user-list-item-"]:first').should("contain",receiverName+" "+recieverLastName).click(); 
+        var transactionName = "Un payement de test";
+        var amount = "185";
+        cy.get('input[name="amount"]').type(amount);
+        cy.get('input[name="description"]').type(transactionName);
+        cy.get('[data-test="transaction-create-submit-payment"]').click();
+        cy.get('[data-test="new-transaction-return-to-transactions"]').click();
+        // recherche le payement
+        cy.get('[data-test="nav-personal-tab"]').click();
+        cy.get('li :first').click();
 
-    /*it('Comment', () =>{
+        cy.get('[data-test^="transaction-sender-"]').should("contain", name+' '+lastName);
+        cy.get('[data-test^="transaction-action-"]').should("contain", 'paid');
+        cy.get('[data-test^="transaction-receiver-"]').should("contain", receiverName+' '+recieverLastName);
+
+        cy.get('[data-test^="transaction-description"]').should("contain", transactionName);
+        cy.get('[data-test^="transaction-amount-"]').should("contain", "-$"+amount);
+        var commentaire = "Mon commentaire de test"
+        cy.get('[data-test^="transaction-comment-input"]').type(commentaire+'{enter}');
+        cy.get('[data-test^="comment-list-item"]:last').should("contain", commentaire);
+
+    });
+
+    it('Comment', () =>{
         cy.get('li :first').click();
         cy.get('[data-test^="transaction-comment-input"]').type("un commentaire{enter}");
         cy.get('[data-test^="comment-list-item"]:last').should("contain", "un commentaire");
 
-    });*/
+    });
 
 
-    // depuis la liste de mes transactions
-    
+    // like + comment depuis les list
 
 
 });
